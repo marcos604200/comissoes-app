e19a3056ee0248efa984c70513c523f766cb1ce3
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-12 00:41:19 -0300

    Update calculo.ts

--- src/pages/api/calculo.ts
+++ src/pages/api/calculo.ts
@@ -1,1 +1,14 @@
 
+import { calcularComissaoCentral } from "@/motores/calcularComissaoCentral";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { dados, regra } = req.body;
+    const resultado = calcularComissaoCentral(dados, regra);
+    return res.status(200).json(resultado);
+  } else {
+    return res.status(405).json({ erro: "Método não permitido" });
+  }
+}
+
