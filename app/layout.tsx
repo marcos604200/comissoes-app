02ca6671e1df2f2fec139061b051b96f4c0b3960
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-12 01:02:56 -0300

    Delete app directory

--- app/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-br">
-      <body className="p-4 bg-gray-100 text-gray-800">{children}</body>
-    </html>
-  );
-}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-2xl font-bold">
-      Bem-vindo ao Sistema de Cálculo de Comissões
-    </div>
-  );
-}
