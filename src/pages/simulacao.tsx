8f6d8843a845373084b885fb0984efcc56d72aaa
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-14 08:22:40 -0300

    Delete src/pages/simulacao.tsx

--- src/pages/simulacao.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "@/components/ui/Layout";
-
-export default function Simulacao() {
-  return (
-    <Layout>
-      <div className="text-xl font-semibold">Tela de Simulação (em breve)</div>
-    </Layout>
-  );
-}
