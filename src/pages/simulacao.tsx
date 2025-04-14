0b7e52a48befd5a4df6dd452f3d42107bb76eea0
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-14 08:28:34 -0300

    Create simulacao.tsx

---
+++ src/pages/simulacao.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+import InterfaceSimulacao from "@/components/InterfaceSimulacao";
+
+export default function Simulacao() {
+  return (
+    <Layout>
+      <InterfaceSimulacao />
+    </Layout>
+  );
+}
