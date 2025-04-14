991ef2cbf26f84d60b32a102e9983655f0e18af2
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-14 20:59:22 -0300

    Update regras.tsx

--- src/pages/regras.tsx
+++ src/pages/regras.tsx
@@ -1,1 +1,11 @@
+import MainLayout from "@/layouts/MainLayout";
+import ConstrutorRegras from "@/components/ConstrutorRegras";
+
+export default function RegrasPage() {
+  return (
+    <MainLayout>
+      <ConstrutorRegras />
+    </MainLayout>
+  );
+}
 
