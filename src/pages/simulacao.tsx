491ea008a05fbfedf8745d5b867f45312a73e8e2
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-13 22:47:23 -0300

    Create simulacao.tsx

---
+++ src/pages/simulacao.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/ui/Layout";
+
+export default function Simulacao() {
+  return (
+    <Layout>
+      <div className="text-xl font-semibold">Tela de Simulação (em breve)</div>
+    </Layout>
+  );
+}
