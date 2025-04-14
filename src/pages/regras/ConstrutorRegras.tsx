30986ca881c401cbc169714ef46c65525d402dae
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-14 08:03:44 -0300

    Create ConstrutorRegras.tsx

---
+++ src/pages/regras/ConstrutorRegras.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+import ConstrutorRegras from "@/components/ConstrutorRegras";
+
+export default function ConstrutorRegrasPage() {
+  return (
+    <Layout>
+      <ConstrutorRegras />
+    </Layout>
+  );
+}
