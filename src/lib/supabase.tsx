2d444612c292aad9e05fc69b2a3c2c220492e7bf
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-14 08:00:35 -0300

    Create supabase.tsx

---
+++ src/lib/supabase.tsx
@@ -0,0 +1,3 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient("https://uythkiygydgezjwzzsnk.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5dGhraXlneWRnZXpqd3p6c25rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0MDMwODAsImV4cCI6MjA1OTk3OTA4MH0.h8KnRf-_Zanr8uP6XnlF3epR8-n_2hpiTwTggR2JgyM");
