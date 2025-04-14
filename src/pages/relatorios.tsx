5fc10594c6a67af7beaa3b56f6159b45a4ea6665
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-13 22:48:31 -0300

    Create relatorios.tsx

---
+++ src/pages/relatorios.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/ui/Layout";
+
+export default function Relatorios() {
+  return (
+    <Layout>
+      <div className="text-xl font-semibold">Relatórios e Gráficos (em breve)</div>
+    </Layout>
+  );
+}
