627b3f47ba42604252f58ed2342894cfe793fc83
marcos604200 <mmescritoriocontabilidade@gmail.com>
2025-04-13 22:47:57 -0300

    Create regras.tsx

---
+++ src/pages/regras.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/ui/Layout";
+import InterfaceSimulacao from "@/components/InterfaceSimulacao";
+
+export default function Regras() {
+  return (
+    <Layout>
+      <InterfaceSimulacao />
+    </Layout>
+  );
+}
